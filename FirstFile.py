47619148a9bb08274ccc63c723f8918b40da60f0
Anthony Arendt <arendta@uw.edu>
2016-02-02 13:50:42 -0800

    functions to clean the netCDF

--- FirstFile.py
+++ FirstFile.py
@@ -2,9 +2,7 @@
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
-import xray
-import xray.ufuncs as xu
-import dask
+import xarray
 #import seaborn as sn
 from datetime import datetime
 from dask.diagnostics import ProgressBar
@@ -17,38 +15,42 @@ warnings.filterwarnings('ignore')
 
 # Create a file list of all the netCDF files
 import glob
-fileList = glob.glob('/datadrive/WRF/radoutd02.nc')
+fileList = glob.glob(r'c:\work\datadrive\WRF\*.nc')
 fileList.sort()
 fileList
 
-# This cell converts concatenates the netCDF files and creates new ones that are better organized
+def clean_netCDF(fileList):
 
-# Create a function that will take the way the time is stored as a variable in the
-# WRF netCDF files and decode it
-# Build a function to go from numpy.bytes_ to a string - WRF files were compressed
-def decode(d):
-    decoded = datetime.strptime(d.decode(encoding='UTF-8'),  "%Y-%m-%d_%H:%M:%S")
-    return decoded
-
-first = True # create a boolean for the loop below
-
+    def decode(d):
+        decoded = datetime.strptime(d.decode(encoding='UTF-8'),  "%Y-%m-%d_%H:%M:%S")
+        return decoded
 
 # Stores the creates a new netCDF files with coordinat
-for file in fileList:
-    print(file)
-    ds = xray.open_dataset(file , engine = 'scipy') # may not need enginer = 'netcdf4' others are using engine = 'scipy'
-    dates = ds.Times.to_series().apply(decode) # this creates a list of the varaibles Times in the netCDF file ds and then applies the decode function to each element
-    dsTotal = xray.Dataset({'prec': (['time','x','y'], ds.prehourly.values),
-                            'temp2m':(['time','x','y'], ds.t2.values)},
-                           coords = {'longitude': (['x','y'], ds.lon.values),
-                                     'latitude': (['x','y'], ds.lat.values),
-                                     'time': xray.DataArray.from_series(dates).values})
+    for file in fileList:
+        print(file)   
+        ds = xarray.open_dataset(file , engine = 'scipy') # may not need enginer = 'netcdf4' others are using engine = 'scipy'
+        dates = ds.Times.to_series().apply(decode) # this creates a list of the varaibles Times in the netCDF file ds and then applies the decode function to each element
+        dsTotal = xarray.Dataset({'prec': (['time','x','y'], ds.prehourly.values),
+                  'temp2m':(['time','x','y'], ds.t2.values)},
+                   coords = {'longitude': (['x','y'], ds.lon.values),
+                          'latitude': (['x','y'], ds.lat.values),
+                          'time': xarray.DataArray.from_series(dates).values})    
     # add attributes to the netCDF
-    dsTotal.attrs['prec'] = 'Precipitation Hourly [mm]'
-    dsTotal.attrs['temp2m'] = 'Two Meter Temperature [deg K]'
+        dsTotal.attrs['prec'] = 'Precipitation Hourly [mm]'
+        dsTotal.attrs['temp2m'] = 'Two Meter Temperature [deg K]'
     # write the netcdf files back to the drive, using the year-month as the name
-    dsTotal.to_netcdf('/datadrive/WRF/temp/' + file.split('_')[2], mode = 'w')
-print('done')
+        dsTotal.to_netcdf('C:\\work\\datadrive\\WRF\\temp2\\' + file.split('_')[2], mode = 'w')
+        print('done')
+
+
+# call this once to create temporary cleaned netCDF files
+
+clean_netCDF(fileList)
+
+dsTotal = xarray.open_mfdataset(r'C:\work\datadrive\WRF\temp2\*.nc')
+dsTotal.chunk({'time':400,'x':50,'y':50})
+
+
 
 
 df = pd.read_csv('DHSVM_example.txt', sep = '\t')
